bc78b12951e61949306a0ea5e7caf31889cffe1f
JulesCombet <jules.combet@etu.univ-amu.fr>
2022-12-01 11:00:44 +0100

    Ajout des attributs time et difficulty qui permettent de connaitre les paramètre qu'a choisi le joueur

--- src/main/java/game/bombParty/Class/Player.java
+++ src/main/java/game/bombParty/Class/Player.java
@@ -1,17 +1,33 @@
 package game.bombParty.Class;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Player {
     private Life life;
+    private Time time;
+    private Difficulty difficulty;
     private Set<Letter> letters;
 
-    public Player(Life life) {
+    public Player(Life life, Time time, Difficulty difficulty) {
         this.life = life;
+        this.time = time;
+        this.difficulty = difficulty;
+        this.letters = new HashSet<>();
         for (Alphabet v: Alphabet.values()) {
             this.letters.add(new Letter(v));
         }
     }
 
+    public Life getLife() {
+        return this.life;
+    }
 
+    public Time getTime() {
+        return this.time;
+    }
+
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
 }
