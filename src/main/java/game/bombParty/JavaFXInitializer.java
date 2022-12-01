8b232bb56977a5fa72c30282ba0f7c1779189c1f
JulesCombet <jules.combet@etu.univ-amu.fr>
2022-12-01 10:37:00 +0100

    Passage des fonctions en static pour pouvoir les appeler dans un autre fichier

--- src/main/java/game/bombParty/JavaFXInitializer.java
+++ src/main/java/game/bombParty/JavaFXInitializer.java
@@ -1,5 +1,7 @@
 package game.bombParty;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -8,9 +10,22 @@ import java.io.IOException;
 
 public interface JavaFXInitializer {
 
-    public StackPane initializeStackPane(StackPane page, boolean isVisible, String file) throws IOException;
+    static StackPane initializeStackPane(StackPane page, boolean isVisible, String file) throws IOException {
+        page = FXMLLoader.load(Main.class.getResource(file));
+        page.setVisible(isVisible);
+        return page;
+    }
 
-    public StackPane initializeMainStackPane(StackPane pane, StackPane... panes);
+    static StackPane initializeMainStackPane(StackPane pane, StackPane... panes) {
+        pane.getChildren().setAll(panes);
+        return pane;
+    }
 
-    public void initializeStage(Stage stage, StackPane main, String title);
+    static void initializeStage(Stage stage, StackPane main, String title) {
+        Scene scene = new Scene(main);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle(title);
+        stage.show();
+    }
 }
