acef0095f58b86a1b930d29252497f39c5d9db16
JulesCombet <jules.combet@etu.univ-amu.fr>
2022-12-01 11:14:06 +0100

    Ajout des fonctions de JavaFXInitializer

--- src/main/java/game/bombParty/Main.java
+++ src/main/java/game/bombParty/Main.java
@@ -1,5 +1,6 @@
 package game.bombParty;
 
+import game.bombParty.Class.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -7,47 +8,45 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.net.URISyntaxException;
 
 public class Main extends Application implements JavaFXInitializer{
 
-    private StackPane homePage;
-    private final StackPane stackPaneMain = new StackPane();
+    private static StackPane homePage;
+    private static StackPane gamePage;
+    private static StackPane mainPane = new StackPane();
+    private static Game game;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        launchAPage(stage, "/game/bombParty/HomePage.fxml");
+    public void start(Stage stage) throws IOException, URISyntaxException {
+        homePage = JavaFXInitializer.initializeStackPane(homePage, true, "/game/bombParty/HomePage.fxml");
+        JavaFXInitializer.initializeMainStackPane(mainPane, homePage);
+        JavaFXInitializer.initializeStage(stage, mainPane, "Bomb Party");
+
+        game = new Game(new Player(new Life(), new Time(), new Difficulty()));
     }
-
-    public void launchAPage(Stage stage, String page) throws IOException {
-        this.homePage = this.initializeStackPane(this.homePage, page);
-        this.initializeMainStackPane(this.stackPaneMain, this.homePage);
-        this.initializeStage(stage, this.stackPaneMain, "Bomb Party");
-
-    }
-
     public static void main(String[] args) {
         launch();
     }
 
-    @Override
-    public StackPane initializeStackPane(StackPane page, String file) throws IOException {
-        page = FXMLLoader.load(Main.class.getResource(file));
-        page.setVisible(true);
-        return page;
+    public static StackPane getHomePage() {
+        return homePage;
     }
 
-    @Override
-    public void initializeMainStackPane(StackPane mainPane, StackPane... panes) {
-        mainPane.getChildren().setAll(panes);
+    public static StackPane getGamePage() {
+        return gamePage;
     }
 
-    @Override
-    public void initializeStage(Stage stage, StackPane main, String title) {
-        Scene scene = new Scene(main);
-        stage.setScene(scene);
-        stage.setResizable(false);
-        stage.setTitle(title);
-        stage.show();
+    public static void setGamePage(StackPane gamePage) {
+        Main.gamePage = gamePage;
     }
+
+    public static StackPane getMainPane() {
+        return mainPane;
+    }
+
+    public static Game getGame() {
+        return game;
+    }
+    /* TODO Remettre en forme */
 }
