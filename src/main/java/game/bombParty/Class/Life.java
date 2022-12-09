f56903b77716e182b093934bd146994ba4734c37
JulesCombet <jules.combet@etu.univ-amu.fr>
2022-12-09 19:58:28 +0100

    Ajout d'un attribut pour bind à un label

--- src/main/java/game/bombParty/Class/Life.java
+++ src/main/java/game/bombParty/Class/Life.java
@@ -1,23 +1,35 @@
 package game.bombParty.Class;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Life {
     private int actualLife;
+    private SimpleStringProperty life = new SimpleStringProperty();
 
     public Life() {
-
+        this.actualLife = 3;
     }
 
     public int getActualLife() {
         return this.actualLife;
     }
 
-    public void looseLife(){
-        if(actualLife > 0) --this.actualLife;
+    public SimpleStringProperty getStringLife(){
+        this.updateStringProperty();
+        return this.life;
     }
 
-    public void winLife(){++this.actualLife;}
+    public void looseLife(){
+        if(actualLife > 0) --this.actualLife;
+        this.updateStringProperty();
+    }
 
-    public void setLife(int life) {
-        this.actualLife = life;
+    private void updateStringProperty() {
+        this.life.set(String.valueOf(this.actualLife));
+    }
+
+    public void winLife() {
+        ++this.actualLife;
+        this.updateStringProperty();
     }
 }
