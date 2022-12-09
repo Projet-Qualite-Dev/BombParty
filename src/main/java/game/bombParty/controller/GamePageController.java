44abd7b570614c4b331f4164b96f8d8204af1bd4
JulesCombet <jules.combet@etu.univ-amu.fr>
2022-12-09 19:52:29 +0100

    Avancement des pages avec leurs controllers

--- src/main/java/game/bombParty/controller/GamePageController.java
+++ src/main/java/game/bombParty/controller/GamePageController.java
@@ -1,58 +1,90 @@
 package game.bombParty.controller;
 
 import game.bombParty.Class.Game;
+import game.bombParty.Class.Life;
+import game.bombParty.Class.Player;
+import game.bombParty.Class.TimeTask;
 import game.bombParty.Main;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.Timer;
 
-public class GamePageController implements Initializable {
+public class GamePageController extends StackPane implements JavaFXControllable {
 
     @FXML
-    public Label syllabLabel, secondLabel;
+    public Label syllabLabel, secondLabel, lifeLabel;
     @FXML
     public TextField textField;
+    private Timer timer;
+    private static Game game;
+    private static int point = 0;
+    private final int time;
+    private static Stage primaryStage;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public GamePageController(Stage primaryStage, int time, int difficulty) throws URISyntaxException, IOException {
+        game = new Game(new Player(new Life()), difficulty);
+        this.loadFXMLFile("Game");
+        this.timer = new Timer();
+        this.time = time;
+        GamePageController.primaryStage = primaryStage;
+        this.lifeLabel.textProperty().bind(game.getStringLife());
+    }
+
+    @FXML
+    public void initialize() {
         this.textField.setOnKeyPressed(event -> {
-            if (event.getCode().equals(KeyCode.ENTER) && !Main.getGame().isRoundTerminated()) {
-                if (Main.getWordList().containsValueByKey(this.syllabLabel.getText(), this.textField.getText().toUpperCase())) {
-                    Main.getGame().getPlayer().getTime().setTime(0);
-                    try {
-                        Main.setGame(new Game(Main.getGame().getPlayer()));
-                    } catch (URISyntaxException | IOException e) {
-                        throw new RuntimeException(e);
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                if (game.containsValue(this.syllabLabel.getText(), this.textField.getText().toUpperCase())) {
+                    this.point++;
+                } else {
+                    if (game.getActualLife() == 0) {
+                        this.returnMenu();
+                    } else {
+                        game.looseLife();
                     }
-                    this.startGame();
                 }
+                this.timer.cancel();
+                this.update();
                 this.textField.setText("");
             }
         });
     }
 
-    public void returnMenu() throws URISyntaxException, IOException {
-        Main.getMainPane().getChildren().setAll(Main.getHomePage());
-        Main.getHomePage().setVisible(true);
-        if (Main.getGame().getState() == Thread.State.TIMED_WAITING) {
-            Main.getGame().getPlayer().getTime().setTime(0);
-        }
-        Main.setGame(new Game(Main.getGame().getPlayer()));
-        /* TODO Arreter le thread */
+    @FXML
+    public void returnMenu() {
+        this.timer.cancel();
+        quit();
+    }
+
+    public static void quit() {
+        primaryStage.setScene(new Scene(new HomePageController(primaryStage, String.valueOf(point))));
     }
 
     @FXML
     public void startGame() {
-        if (Main.getGame().getState() != Thread.State.RUNNABLE) {
-            Main.getGame().setLabels(this.syllabLabel, this.secondLabel);
-            Main.getGame().start();
+        this.textField.requestFocus();
+        if (!game.isStarted()) {
+            game.start();
+            this.update();
         }
     }
+
+    public static Game getGame() {
+        return game;
+    }
+
+    private void update() {
+        this.syllabLabel.setText(game.getRandomSyllab());
+        this.timer = new Timer();
+        this.timer.scheduleAtFixedRate(new TimeTask(this.time, this.secondLabel, this.syllabLabel, game.getLife()), 0, 100);
+    }
+    /* TODO Rajouter le temps d'execution faire un attribut dans game ou wordMap*/
 }
--- src/main/java/game/bombParty/controller/HomePageController.java
+++ src/main/java/game/bombParty/controller/HomePageController.java
@@ -1,31 +1,39 @@
 package game.bombParty.controller;
 
-import game.bombParty.Class.Difficulty;
-import game.bombParty.Class.Life;
-import game.bombParty.Class.Player;
-import game.bombParty.Class.Time;
-import game.bombParty.JavaFXInitializer;
 import game.bombParty.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HomePageController implements Initializable {
+public class HomePageController extends StackPane implements JavaFXControllable {
     @FXML
     Slider lifeSlider, timeSlider, difficultySlider;
     @FXML
-    Label lifeLabel, timeLabel, difficultyLabel;
+    Label lifeLabel, timeLabel, difficultyLabel, scoreLabel;
+    private Stage primaryStage;
 
+    public HomePageController(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.loadFXMLFile("Home");
+    }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public HomePageController(Stage primaryStage, String scrore) {
+        this.primaryStage = primaryStage;
+        this.loadFXMLFile("Home");
+        this.scoreLabel.setText(scrore);
+    }
+
+    @FXML
+    public void initialize() {
         this.initializeSlider(this.lifeSlider, 1, 4, this.lifeLabel);
         this.initializeSlider(this.timeSlider, 5, 12, this.timeLabel);
         this.initializeSlider(this.difficultySlider, 1, 3, this.difficultyLabel);
@@ -33,12 +41,8 @@ public class HomePageController implements Initializable {
 
     @FXML
     public void launchGamePage() throws IOException, URISyntaxException {
-        Main.getGame().setLife(Integer.parseInt(this.lifeLabel.getText()));
-        Main.getGame().setTime(Integer.parseInt(this.timeLabel.getText()));
-        Main.getGame().setDifficulty(Integer.parseInt(this.difficultyLabel.getText()));
-
-        Main.setGamePage(JavaFXInitializer.initializeStackPane(Main.getGamePage(), true, "/game/bombParty/GamePage.fxml"));
-        Main.getMainPane().getChildren().setAll(Main.getGamePage());
+        Scene scene = new Scene(new GamePageController(this.primaryStage, (int) this.timeSlider.getValue(), Integer.parseInt(this.difficultyLabel.getText())));
+        this.primaryStage.setScene(scene);
     }
 
     private void initializeSlider(Slider slider, Integer min, Integer max, Label label) {
