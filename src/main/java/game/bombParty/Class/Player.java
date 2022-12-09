10b10bc729fbdb714b49e64fd03962d0f75ccfb0
JulesCombet <jules.combet@etu.univ-amu.fr>
2022-12-09 19:58:06 +0100

    Ajout de fonctions getters

--- src/main/java/game/bombParty/Class/Player.java
+++ src/main/java/game/bombParty/Class/Player.java
@@ -1,33 +1,39 @@
 package game.bombParty.Class;
 
+import javafx.beans.property.SimpleStringProperty;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class Player {
     private Life life;
-    private Time time;
-    private Difficulty difficulty;
     private Set<Letter> letters;
 
-    public Player(Life life, Time time, Difficulty difficulty) {
+    public Player(Life life) {
         this.life = life;
-        this.time = time;
-        this.difficulty = difficulty;
         this.letters = new HashSet<>();
         for (Alphabet v: Alphabet.values()) {
             this.letters.add(new Letter(v));
         }
     }
 
+    public int getActualLife() {
+        return this.life.getActualLife();
+    }
+
+    public void winLife() {
+        this.life.winLife();
+    }
+
+    public void looseLife() {
+        this.life.looseLife();
+    }
+
+    public SimpleStringProperty getStringLife() {
+        return this.life.getStringLife();
+    }
+
     public Life getLife() {
         return this.life;
-    }
-
-    public Time getTime() {
-        return this.time;
-    }
-
-    public Difficulty getDifficulty() {
-        return this.difficulty;
     }
 }
