b51cc2bb251931eaca0648f629da3a28e534fb75
gabriel-languille <gabriel.languille@etu.univ-amu.fr>
2022-12-13 14:59:58 +0100

    Ajout de la javadoc

--- src/main/java/game/bombParty/Class/TimeTask.java
+++ src/main/java/game/bombParty/Class/TimeTask.java
@@ -1,7 +1,3 @@
-/**
- * Cette class permet de gérer le décompte lors du jeu.
- */
-
 package game.bombParty.Class;
 
 import game.bombParty.controller.GamePageController;
@@ -10,13 +6,34 @@ import javafx.scene.control.Label;
 
 import java.util.TimerTask;
 
+/**
+ * Cette class permet de gérer le décompte lors du jeu.
+ */
 public final class TimeTask extends TimerTask {
 
+    /**
+     * Les secondes restantes avant la fin.
+     */
     private int second;
-    private int millisecond;
+    /**
+     * Les dixièmes de secondes avant la fin.
+     */
+    private int tenthOfASecond;
+    /**
+     * Les secondes de départ pour le décompte.
+     */
     private final int TIME;
+    /**
+     * Le Label qui permet d'afficher le temps restant.
+     */
     private final Label SECOND_LABEL;
+    /**
+     * Le Label qui permet d'afficher la syllabe en cours.
+     */
     private final Label SYLLABLE;
+    /**
+     * La vie du joueur.
+     */
     private final Life LIFE;
 
     /**
@@ -30,7 +47,7 @@ public final class TimeTask extends TimerTask {
     public TimeTask(int time, Label secondLabel, Label syllableLabel, Life life) {
         this.TIME = time;
         this.second = this.TIME;
-        this.millisecond = 0;
+        this.tenthOfASecond = 0;
         this.SECOND_LABEL = secondLabel;
         this.SYLLABLE = syllableLabel;
         this.LIFE = life;
@@ -41,14 +58,14 @@ public final class TimeTask extends TimerTask {
      */
     @Override
     public void run() {
-        if (this.millisecond == 0) {
+        if (this.tenthOfASecond == 0) {
             this.second--;
-            this.millisecond = 9;
+            this.tenthOfASecond = 9;
         } else {
-            this.millisecond -= 1;
+            this.tenthOfASecond -= 1;
         }
-        if (this.second == 0 && this.millisecond == 0) {
-            if (this.LIFE.getActualLife() == 0) {
+        if (this.second == 0 && this.tenthOfASecond == 0) {
+            if (this.LIFE.getActualLife() == 0) { // Si le joueur n'a plus de vies
                 this.cancel();
                 Platform.runLater(GamePageController::quit);
             } else {
@@ -60,6 +77,6 @@ public final class TimeTask extends TimerTask {
             }
 
         }
-        Platform.runLater(() -> this.SECOND_LABEL.setText(this.second + "." + this.millisecond + "s"));
+        Platform.runLater(() -> this.SECOND_LABEL.setText(this.second + "." + this.tenthOfASecond + "s"));
     }
 }
