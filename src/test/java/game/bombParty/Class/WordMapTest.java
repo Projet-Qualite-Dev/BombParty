c293ce2d9d61223b4ebb954c710afeaaac5dda30
Lucas-Rodrigues <lucas.rodrigues@etu.univ-amu.fr>
2022-12-14 15:14:52 +0100

    ajout des test de la class WordMap

--- src/test/java/game/bombParty/Class/WordMapTest.java
+++ src/test/java/game/bombParty/Class/WordMapTest.java
@@ -1,4 +1,31 @@
 package game.bombParty.Class;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class WordMapTest {
+
+    @Test
+    void constructorTest() throws Exception {
+        WordMap wordMap = new WordMap(false);
+        Assertions.assertNotNull(wordMap);
+    }
+
+    @Test
+    void getExecuteTimeTest() throws Exception {
+        WordMap wordMap = new WordMap(false);
+        Assertions.assertTrue(WordMap.getExecuteTime().startsWith("Temps d'execution de la lecture du dictionnaire (en seconde) : "));
+    }
+
+    @Test
+    void getRandomSyllableTest() throws Exception {
+        WordMap wordMap = new WordMap(false);
+        Assertions.assertEquals(wordMap.getRandomSyllable(1).length(), 3);
+    }
+
+    @Test
+    void containsValueByKeyTest() throws Exception {
+        WordMap wordMap = new WordMap(false);
+        Assertions.assertTrue(wordMap.containsValueByKey("NJO", "BONJOUR"));
+    }
 }
