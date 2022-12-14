8445814ec79a8f40a4d6af306f10e1e4b85e0eea
Lucas-Rodrigues <lucas.rodrigues@etu.univ-amu.fr>
2022-12-14 11:38:14 +0100

    ajout des test de la class Life

--- src/test/java/game/bombParty/Class/LifeTest.java
+++ src/test/java/game/bombParty/Class/LifeTest.java
@@ -1,4 +1,41 @@
 package game.bombParty.Class;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class LifeTest {
+
+    @Test
+    void constructorTest() {
+        Life life = new Life();
+        Assertions.assertNotNull(life);
+    }
+
+    @Test
+    void getActualLifeTest() {
+        Life life = new Life();
+        Assertions.assertEquals(life.getActualLife(),3);
+    }
+
+    @Test
+    void getStringLifeTest() {
+        Life life = new Life();
+        Assertions.assertEquals(life.getStringLife().toString(), new SimpleStringProperty("3").toString() );
+    }
+
+    @Test
+    void winLifeTest() {
+        Life life = new Life();
+        life.winLife();
+        Assertions.assertEquals(life.getActualLife(),4);
+    }
+
+    @Test
+    void looseLifeTest() {
+        Life life = new Life();
+        life.looseLife();
+        Assertions.assertEquals(life.getActualLife(), 2);
+    }
 }
