28a2e78eae165467f83f71edfa1eb57c703557e9
Lucas-Rodrigues <lucas.rodrigues@etu.univ-amu.fr>
2022-12-14 13:29:25 +0100

    ajout des de la class Difficulty

--- src/test/java/game/bombParty/Class/DifficultyTest.java
+++ src/test/java/game/bombParty/Class/DifficultyTest.java
@@ -1,4 +1,24 @@
 package game.bombParty.Class;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DifficultyTest {
+
+    @Test
+    void constructorTest() throws Exception {
+        Difficulty difficulty = new Difficulty(3);
+        Assertions.assertNotNull(difficulty);
+    }
+
+    @Test
+    void constructorTest2() throws Exception {
+        Assertions.assertThrows(Exception.class, () -> new Difficulty(8));
+    }
+
+    @Test
+    void getValueTest() throws Exception {
+        Difficulty difficulty = new Difficulty(3);
+        Assertions.assertEquals(difficulty.getValue(), 50);
+    }
 }
