8b1838cfb5e9759d661b218a9aa9eab4d5ba5317
Lucas-Rodrigues <lucas.rodrigues@etu.univ-amu.fr>
2022-12-14 11:55:56 +0100

    ajout des test pour la class AllLettersForGame

--- src/test/java/game/bombParty/Class/AllLettersForGameTest.java
+++ src/test/java/game/bombParty/Class/AllLettersForGameTest.java
@@ -1,4 +1,17 @@
 package game.bombParty.Class;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AllLettersForGameTest {
+
+    @Test
+    void constructorAndGetterTest() {
+        Assertions.assertEquals(AllLettersForGame.A.getValue(), "A");
+    }
+
+    @Test
+    void getValuesTest() {
+        Assertions.assertEquals(AllLettersForGame.values().length, 21);
+    }
 }
