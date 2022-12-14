393daa6f5ee8be0c311b5cce86ef8a46f9edd88e
Lucas05282003 <lucas.rodrigues@etu.univ-amu.fr>
2022-12-14 11:08:02 +0100

    ajout des test de player

--- src/test/java/game/bombParty/Class/PlayerTest.java
+++ src/test/java/game/bombParty/Class/PlayerTest.java
@@ -1,4 +1,38 @@
 package game.bombParty.Class;
 
+import javafx.beans.property.SimpleStringProperty;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PlayerTest {
+
+    @Test
+    void constructorTest() {
+        Player player = new Player(new Life());
+        Assertions.assertNotNull(player);
+    }
+
+    @Test
+    void getLifeTest() {
+        Life life = new Life();
+        Player player = new Player(life);
+        Assertions.assertEquals(player.getLife() , life);
+    }
+
+
+    @Test
+    void getStringLifeTest() {
+        Life life = new Life();
+        Player player = new Player(life);
+        Assertions.assertEquals(player.getStringLife().toString() , new SimpleStringProperty("3").toString());
+    }
+
+    @Test
+    void winLifeTest() {
+        Life life = new Life();
+        Player player = new Player(life);
+        player.winLife();
+        Assertions.assertEquals(player.getLife().getActualLife() , 4);
+    }
+
 }
