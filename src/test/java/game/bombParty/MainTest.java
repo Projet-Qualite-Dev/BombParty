7610ab5af900e47c20d0ab1dd9d5c4e6347bbf91
Jules Combet <jules.combet@etu.univ-amu.fr>
2022-12-15 18:48:06 +0100

    Ajout du test pour la class Main

--- src/test/java/game/bombParty/MainTest.java
+++ src/test/java/game/bombParty/MainTest.java
@@ -1,4 +1,15 @@
 package game.bombParty;
 
+import javafx.application.Application;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+
+    @Test
+    public void testStart() {
+        Main main = new Main();
+        Assertions.assertNotNull(main);
+        Assertions.assertTrue(main instanceof Application);
+    }
 }
